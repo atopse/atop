a0a2222d4fb60a64f0e8fa7e9db9c06379f26455
ysqi <devysq@gmail.com>
2016-09-13 20:56:01 +0800

    APP 初始化修改

--- worker/app/app.go
+++ worker/app/app.go
@@ -1,123 +1,76 @@
 package app
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
-	"github.com/astaxie/beego/logs"
 	"github.com/nsqio/go-nsq"
+
+	"log"
+
 	"github.com/ysqi/atop/common"
+	"github.com/ysqi/atop/common/config"
+	"github.com/ysqi/atop/common/message"
 )
-
-// NSQLoger output nsq log info.
-type NSQLoger struct {
-	log *logs.BeeLogger
-}
-
-// Output nsq log with BeeLogger.
-func (nlog *NSQLoger) Output(calldepth int, s string) error {
-	nlog.log.Write([]byte("[NSQ]" + s))
-	return nil
-}
-
-func newTopicDiscoverer(cfg *nsq.Config) *TopicDiscoverer {
-	return &TopicDiscoverer{
-		workers:  make(map[string]*ConsumerWorker),
-		termChan: make(chan os.Signal),
-		hupChan:  make(chan os.Signal),
-		cfg:      cfg,
-	}
-}
-
-func newConsumerWorker(topic string, channel string, cfg *nsq.Config) (*ConsumerWorker, error) {
-	f := &ConsumerWorker{
-		Topic:    topic,
-		msgChan:  make(chan *nsq.Message, 1),
-		ExitChan: make(chan int),
-		termChan: make(chan bool),
-	}
-	consumer, err := nsq.NewConsumer(topic, channel, cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	consumer.AddHandler(f)
-	consumer.SetLogger(nsqLogger, nsq.LogLevelInfo)
-
-	err = consumer.ConnectToNSQDs(nsqdTCPAddrs)
-	if err != nil {
-		logs.Warn("连接 NSQDS 失败,", err)
-	}
-	err = consumer.ConnectToNSQLookupds(lookupdHTTPAddrs)
-	if err != nil {
-		logs.Warn("连接 NSQLookupd 失败,", err)
-	}
-
-	f.C = consumer
-	return f, nil
-}
 
 var (
-	topicHandles = make(map[string]nsq.HandlerFunc)
-
-	discoverer       *TopicDiscoverer
-	nsqLogger        *NSQLoger
-	nsqdTCPAddrs     []string
+	topicHandles     = make(map[string]nsq.HandlerFunc)
+	discoverer       *message.TopicDiscoverer
+	nsqdTCPAddr      string
 	lookupdHTTPAddrs []string
+	nsqCfg           *nsq.Config
+)
+var (
+	// ProduceWorker 用于发送信息到 NSQD
+	ProduceWorker *message.ProduceWorker
 )
 
-// RegHandle Reg a new topic handle.
+// AddHandle Add a new topic handle.
 // 此说明有可实现的 Handle 来处理 Topic 信息,因此将订阅该主题处理消息
-func RegHandle(topic string, h nsq.HandlerFunc) {
-	if _, ok := topicHandles[topic]; ok {
-		panic(topic + "  已注册,不允许重复")
+func AddHandle(topic string, h nsq.HandlerFunc) error {
+	return discoverer.SubTopic(topic, h)
+}
+func init() {
+
+	channel := config.AppCfg.DefaultString("channel", "main")
+
+	nsqdTCPAddr = config.AppCfg.String("nsqd-tcp-addr")
+	lookupdHTTPAddrs = config.AppCfg.Strings("lookupd-http-address")
+	if nsqdTCPAddr == "" {
+		log.Fatalln("必需配置 nsqd-tcp-addrs")
 	}
-	if h == nil {
-		panic("此 Handle 必需存在")
+	if len(lookupdHTTPAddrs) == 0 {
+		log.Fatalln("必需配置 lookupd-http-address")
 	}
-	topicHandles[topic] = h
+
+	nsqCfg = nsq.NewConfig()
+	nsqCfg.UserAgent = fmt.Sprintf("atop_WORKER/%s go-nsq/%s", VERSION, nsq.VERSION)
+	nsqCfg.MaxInFlight = config.AppCfg.DefaultInt("maxInFlight", 10)
+	if dialTimeout := time.Duration(config.AppCfg.DefaultInt("dialTimeout", 0)); dialTimeout > 0 {
+		nsqCfg.DialTimeout = dialTimeout
+	}
+	discoverer = message.NewTopicDiscoverer(nsqCfg, channel, nil, lookupdHTTPAddrs)
 }
 
 // Run APP
 func Run() error {
 	common.RunStartHook()
-
-	if len(topicHandles) == 0 {
-		return errors.New("无 Topic Handle 加载,退出程序")
+	var err error
+	ProduceWorker, err = message.NewProducer(nsqdTCPAddr, nsqCfg)
+	if err != nil {
+		return err
 	}
+	go discoverer.Start()
 
-	channel := common.AppCfg.DefaultString("channel", "main")
-
-	nsqdTCPAddrs = common.AppCfg.Strings("nsqd-tcp-addrs")
-	lookupdHTTPAddrs = common.AppCfg.Strings("lookupd-http-address")
-	if len(nsqdTCPAddrs) == 0 && len(lookupdHTTPAddrs) == 0 {
-		return errors.New("必需配置 nsqd-tcp-addrs 或 lookupd-http-address")
-	}
-
-	nsqLogger = &NSQLoger{logs.GetBeeLogger()}
-	nsqCfg := nsq.NewConfig()
-	nsqCfg.UserAgent = fmt.Sprintf("atop_WORKER/%s go-nsq/%s", VERSION, nsq.VERSION)
-	nsqCfg.MaxInFlight = common.AppCfg.DefaultInt("maxInFlight", 10)
-	if dialTimeout := time.Duration(common.AppCfg.DefaultInt("dialTimeout", 0)); dialTimeout > 0 {
-		nsqCfg.DialTimeout = dialTimeout
-	}
-
-	discoverer = newTopicDiscoverer(nsqCfg)
-
-	signal.Notify(discoverer.hupChan, syscall.SIGHUP)
-	signal.Notify(discoverer.termChan, syscall.SIGINT, syscall.SIGTERM)
-
-	discoverer.watch(channel)
 	return nil
 }
 
 // Stop App
 func Stop() {
 	if discoverer != nil {
-		discoverer.termChan <- syscall.SIGSTOP
+		discoverer.Exit()
+	}
+	if ProduceWorker != nil {
+		ProduceWorker.Exit()
 	}
 }
--- worker/app/app_test.go
+++ worker/app/app_test.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"testing"
-	"time"
 
 	"github.com/nsqio/go-nsq"
 )
@@ -10,29 +9,13 @@ import (
 func TestAppRun(t *testing.T) {
 	topics := []string{"test", "t2", "t3", "t4"}
 	for _, tp := range topics {
-		RegHandle(tp, func(m *nsq.Message) error {
-			t.Log(tp + ":" + string(m.Body))
+		AddHandle(tp, func(m *nsq.Message) error {
 			return nil
 		})
 	}
-	exitC := make(chan bool, 1)
-	go func() {
-		timer := time.NewTimer(2 * time.Second)
-		for {
-			select {
-			case <-timer.C:
-				if len(discoverer.workers) != len(topics) {
-					t.Fatalf("lenght of discoverer.workers  want %d,got %d", len(topics), len(discoverer.workers))
-				}
-				Stop()
-				return
-			case <-exitC:
-				return
-			}
-		}
-	}()
 	if err := Run(); err != nil {
 		t.Error(err)
-		exitC <- true
+	} else {
+		Stop()
 	}
 }
