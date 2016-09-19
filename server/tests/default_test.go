da1fe07c08d6ee29fe2be271ff63f9c7e4c12025
ysqi <devysq@gmail.com>
2016-09-19 20:03:17 +0800

    修改Test信息

--- server/tests/default_test.go
+++ server/tests/default_test.go
@@ -1,56 +1,20 @@
 package test
 
 import (
-	"net/http"
-	"net/http/httptest"
-	"path/filepath"
-	"runtime"
-	"testing"
-
-	_ "github.com/ysqi/atop/server/routers"
+	"github.com/ysqi/atop/common"
+	"github.com/ysqi/atop/common/config"
+	"github.com/ysqi/atop/common/log2"
+	_ "github.com/ysqi/atop/server/controllers/routers"
 
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/utils"
-	. "github.com/smartystreets/goconvey/convey"
 )
 
-func getConfigPath() string {
-	_, file, _, _ := runtime.Caller(1)
-	dir := filepath.Dir(file)
-	for {
-		d := filepath.Join(dir, "conf", "app.conf")
-		if dir == filepath.VolumeName(dir) {
-			return d
-		}
-		if utils.FileExists(d) {
-			return d
-		}
-		// Parent dir.
-		dir = filepath.Dir(dir)
-	}
-}
-
 func init() {
-	// _, file, _, _ := runtime.Caller(1)
-	// apppath, _ := filepath.Abs(filepath.Dir(filepath.Join(file, ".."+string(filepath.Separator))))
-	appPath := getConfigPath()
-	beego.TestBeegoInit(filepath.Dir(filepath.Dir(appPath))) 
-}
-
-// TestMain is a sample to run an endpoint test
-func TestMain(t *testing.T) {
-	r, _ := http.NewRequest("GET", "/", nil)
-	w := httptest.NewRecorder()
-	beego.BeeApp.Handlers.ServeHTTP(w, r)
-
-	beego.Trace("testing", "TestMain", "Code[%d]\n%s", w.Code, w.Body.String())
-
-	Convey("Subject: Test Station Endpoint\n", t, func() {
-		Convey("Status Code Should Be 200", func() {
-			So(w.Code, ShouldEqual, 200)
-		})
-		Convey("The Result Should Not Be Empty", func() {
-			So(w.Body.Len(), ShouldBeGreaterThan, 0)
-		})
+	log2.Info("应用配置文件:", config.AppCfgPath)
+	beego.AddAPPStartHook(func() error {
+		common.RunStartHook()
+		return nil
 	})
+	beego.InitBeegoBeforeTest(config.AppCfgPath)
+	beego.SetLogFuncCall(false)
 }
